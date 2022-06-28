a8334f3a78be8624307f1864d26ab5cc2b534ee2
iliescumichele <iliescumichele@gmail.com>
2022-06-28 14:46:59 +0200

    Aggiunta dei contenuti come partials per il layout

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,79 @@
+<footer>
+    <div class="top-foot">
+        <div class=" container clearfix">
+            <div class="col-sx">
+                <strong>DC COMICS</strong>
+                <ul>
+                    <li><a href="">Characers</a></li>
+                    <li><a href="">Comics</a></li>
+                    <li><a href="">Movies</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">Games</a></li>
+                    <li><a href="">Videos</a></li>
+                    <li><a href="">News</a></li>
+                </ul>
+
+                <strong>SHOP</strong>
+                <ul>
+                    <li><a href="">Shop DC</a></li>
+                    <li><a href="">Shop DC Collectibles</a></li>
+                </ul>
+            </div>
+            <div class="col-center">
+                <strong>DC</strong>
+                <ul>
+                    <li><a href="">Terms of Use</a></li>
+                    <li><a href="">Privacy policy</a></li>
+                    <li><a href="">Ad choices</a></li>
+                    <li><a href="">advertising</a></li>
+                    <li><a href="">Jobs</a></li>
+                    <li><a href="">Subscription</a></li>
+                    <li><a href="">Talent Workshop</a></li>
+                    <li><a href="">CPSC certification</a></li>
+                    <li><a href="">Ratings</a></li>
+                    <li><a href="">Shop Help</a></li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-dx">
+                <strong>SITES</strong>
+                <ul>
+                    <li><a href="">Characers</a></li>
+                    <li><a href="">Comics</a></li>
+                    <li><a href="">Movies</a></li>
+                    <li><a href="">TV</a></li>
+                    <li><a href="">Games</a></li>
+                    <li><a href="">Videos</a></li>
+                    <li><a href="">News</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="bottom-foot">
+        <div class="container">
+            <div class="btn-signup">
+                <a href="">SIGN-UP NOW!</a>
+            </div>
+
+            <div class="follow-other-platforms">
+                <strong>FOLLOW US</strong>
+                <a href="">
+                    <img src="../assets/img/footer-facebook.png" alt="">
+                </a>
+                <a href="">
+                    <img src="../assets/img/footer-twitter.png" alt="">
+                </a>
+                <a href="">
+                    <img src="../assets/img/footer-youtube.png" alt="">
+                </a>
+                <a href="">
+                    <img src="../assets/img/footer-pinterest.png" alt="">
+                </a>
+                <a href="">
+                    <img src="../assets/img/footer-periscope.png" alt="">
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header class="">
+    <div class="container ">
+        <div class="logo">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+        <div class="nav">
+            <ul>
+                <li><a href=""> CHARACTERS</a></li>
+                <li class="active"><a href=""> COMICS</a></li>
+                <li><a href=""> MOVIES</a></li>
+                <li><a href=""> TV</a></li>
+                <li><a href=""> GAMES</a></li>
+                <li><a href=""> COLLECTIBLES</a></li>
+                <li><a href=""> VIDEOS</a></li>
+                <li><a href=""> FANS</a></li>
+                <li><a href=""> NEWS</a></li>
+                <li><a href=""> SHOP</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
